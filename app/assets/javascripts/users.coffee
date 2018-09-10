f4f31ef5e550bfd0235d5854b04f5603c2efd457
Shubham <shubham.verma@symbtechnologies.com>
2018-09-10 18:16:09 +0530

    make call hide implementation

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -1,20 +1,14 @@
 window.callbutton = (customernumber)->
   $.ajax(
-    url: 'https://kpi.knowlarity.com/Basic/v1/account/call/makecall'
-    type: 'POST'
+    url: 'call?id=' + customernumber
+    type: 'get'
     dataType: 'json'
-    data: JSON.stringify({
-      k_number: '+912248770336',
-      agent_number: '+918010481221',
-      customer_number: '+91' + customernumber
-    })
     headers: (
-      "x-api-key": "x46Q8Td7D67Pd7ePSjCXu9UnZYeDk1la9uCulUmU",
-      "Authorization": "a4fc9ffd-f32a-4225-a358-8246ab9fb8da",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Accept": "application/json"
     )
     success: (data) ->
-      alert "Call success"
-    error: ->
+      alert data
+    error: (error) ->
       alert "Something went wrong"
   )
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  include HTTParty
+
   def index
     @users = User.all
   end
@@ -13,6 +15,37 @@ class UsersController < ApplicationController
     if user.save
       redirect_to '/'
     end
+  end
+
+  def makecall
+    user = User.find(params[:id])
+    # try do
+    base_uri = Uri.parse("https://kpi.knowlarity.com/")
+    http = Net::HTTP.new(base_uri.host, base_uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.initialize_http_header(:headers => {'Content-Type' => 'application/json',
+                                                'accept' => 'application/json',
+                                                'authorization' => 'a4fc9ffd-f32a-4225-a358-8246ab9fb8da',
+                                                'x-api=key' => 'x46Q8Td7D67Pd7ePSjCXu9UnZYeDk1la9uCulUmU'})
+    request.set_form_data({"k_number" => "+912248770336", "agent_number" => "+918010481221", "customer_number" => "+91" + user.mobile})
+
+    @dataresult = http.request(request)
+      # self.class.pem(File.read("#{File.expand_path('.')}/app/assets/k.pem"))
+      # @dataresult = self.class.post('Basic/v1/account/call/makecall',
+      #                               :body => {:k_number => '+912248770336',
+      #                                         :agent_number => '+918010481221',
+      #                                         :customer_number => '+91' + user.mobile
+      #                               }.to_json,
+      #                               :headers => {'Content-Type' => 'application/json',
+      #                                            'accept' => 'application/json',
+      #                                            'authorization' => 'a4fc9ffd-f32a-4225-a358-8246ab9fb8da',
+      #                                            'x-api=key' => 'x46Q8Td7D67Pd7ePSjCXu9UnZYeDk1la9uCulUmU'})
+    # end
+    # catch(exception: Exception) do
+    # @result = exception
+    # end
+    # render json: @result.as_json
+    # render json: {message: 'message', data: result, httpscode: 200}
   end
 
   private def user_params

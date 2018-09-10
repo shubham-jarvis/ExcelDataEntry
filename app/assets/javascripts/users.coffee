56f079ed05da1667bee79e7e1631bd0e96c9221f
Shubham <shubham.verma@symbtechnologies.com>
2018-09-10 11:27:33 +0530

    cloud calling api implementation

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -1,3 +1,20 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+window.callbutton = (customernumber)->
+  $.ajax(
+    url: 'https://kpi.knowlarity.com/Basic/v1/account/call/makecall'
+    type: 'POST'
+    dataType: 'json'
+    data: JSON.stringify({
+      k_number: '+912248770336',
+      agent_number: '+918010481221',
+      customer_number: '+91' + customernumber
+    })
+    headers: (
+      "x-api-key": "x46Q8Td7D67Pd7ePSjCXu9UnZYeDk1la9uCulUmU",
+      "Authorization": "a4fc9ffd-f32a-4225-a358-8246ab9fb8da",
+      "Content-Type": "application/json"
+    )
+    success: (data) ->
+      alert "Call success"
+    error: ->
+      alert "Something went wrong"
+  )
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_headers
+
+  def set_headers
+    if request.headers["HTTP_ORIGIN"]
+      # better way check origin
+      # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
 end

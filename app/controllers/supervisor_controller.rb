addf4e408c97fa4ded7f2f1e33698cba4d1a4122
Shubham <shubham.verma@symbtechnologies.com>
2018-09-12 15:28:15 +0530

    some changes

--- app/controllers/supervisor_controller.rb
+++ app/controllers/supervisor_controller.rb
@@ -29,14 +29,10 @@ class SupervisorController < ApplicationController
   end
 
   def assign
-    Customer.where(currently_assigned_to: nil).each do |c|
-      User.where(role_id: 3).find_each do |i|
-        if Customer.where(currently_assigned_to: i.id).count <= 5
-          c.currently_assigned_to = i.id
     Customer.where(currently_assigned_to: nil, is_complete: false).each do |c|
       User.where(role_id: Role.find_by_title("Agent").id).find_each do |i|
         if Customer.where(currently_assigned_to: i.id, is_complete: false).count <= 5
-           c.currently_assigned_to = i.id
+          c.currently_assigned_to = i.id
           c.save
         end
       end
@@ -44,22 +40,5 @@ class SupervisorController < ApplicationController
     @@i = 1
     redirect_to supervisor_index_path
   end
-
- def import
-   keys=["Organization", "State", "Designation", "Name", "Gender", "Mobile_No", "Whatsapp_No", "Email", "Address", "Village", "Taluka", "District", "Pincode", "Vidhan_Sabha", "Booth", "Primary_Member_ID", "Voter_ID", "Caste", "Category", "Education", "Profession", "currently_assigned_to"]
-   myfile = params[:file]
-   @rowarraydisp = CSV.read (myfile.path)
-   @rowarraydisp.each do |row|
-     puts Customer.create(Hash[keys.zip(row)])
-   end
-   Customer.where(currently_assigned_to: nil).each do |c|
-     User.where(role_id: 3).find_each do |i|
-       if Customer.where(currently_assigned_to: i.id).count <= 5
-         c.currently_assigned_to = i.id
-         c.save
-       end
-     end
-   end
- end
 
 end
